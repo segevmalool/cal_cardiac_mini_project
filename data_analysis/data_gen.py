4c9bedf3e6366f2b571110ef215f6769d10e3738
segevmalool <segevmalool@yahoo.com>
2017-12-09 21:59:58 -0800

    added django

--- data_analysis/data_gen.py
+++ data_analysis/data_gen.py
@@ -12,7 +12,7 @@ class ParseArrhythmiaSignals(object):
 
     The data batches are windows centered around beat annotations. The size of the window is controlled by the
     window_size parameter"""
-    def __init__(self, window_size):
+    def __init__(self, window_size, record_num):
         """For each record number, there is a data sample with metadata, and an annotation. In aggregate, these
         are stored in the all_patients_data dict. The key is the record number, and the value is a pair or python
         objects for the data signals and annotations.
@@ -29,41 +29,43 @@ class ParseArrhythmiaSignals(object):
         self.alpha = 0.5
 
         self.read_all_records()
+        
+        self.active_record = record_num
 
     def read_all_records(self):
         """Aggregates data from all records into a [m,n,2] pseudoimage"""
         for r in self.record_numbers:
-            self.read_single_record(r)
+            self.read_single_record()
 
-    def read_single_record(self, record_num):
+    def read_single_record(self):
         """Updates the records dictionary with data from a given record number. 
         The data_dir should contain the .atr, .dat, and .hea files."""
         try:
-            sample = wfdb.rdsamp(self.data_dir + record_num)
-            annot = wfdb.rdann(self.data_dir + record_num, 'atr')
+            sample = wfdb.rdsamp(self.data_dir + self.active_record)
+            annot = wfdb.rdann(self.data_dir + self.active_record, 'atr')
         except ValueError:
             raise ValueError('Record not found') 
 
         # validation_index = sci.random.choice(range(len(annot.sample)))
-        self.all_patients_data[str(record_num)] = (sample, annot)
+        self.all_patients_data[str(self.active_record)] = (sample, annot)
 
-    def get_data_obj(self, record_num):
+    def get_data_obj(self):
         """Returns the data object for the given record."""
-        return self.all_patients_data[str(record_num)][0]
+        return self.all_patients_data[str(self.active_record)][0]
 
-    def get_annot_obj(self, record_num):
+    def get_annot_obj(self):
         """Returns the data object for the given record."""
-        return self.all_patients_data[str(record_num)][1]
+        return self.all_patients_data[str(self.active_record)][1]
 
-    def generate_data_batch(self, record_num, n=100, channel=0):
+    def generate_data_batch(self, n=100, channel=0):
         """Returns data batch of ECG signals centered on heartbeat. Default n=100, channel=1."""
-        if not self.all_patients_data[str(record_num)]:
-            print('Patient number ' + str(record_num) + 'has not been read')
+        if not self.all_patients_data[str(self.active_record)]:
+            print('Patient number ' + str(self.active_record) + 'has not been read')
             return
         else:
-            ch = sci.array(self.get_data_obj(record_num).adc()[:, channel])
-            idx = sci.array(self.get_annot_obj(record_num).sample)
-            label = sci.array(self.get_annot_obj(record_num).symbol)
+            ch = sci.array(self.get_data_obj(self.active_record).adc()[:, channel])
+            idx = sci.array(self.get_annot_obj(self.active_record).sample)
+            label = sci.array(self.get_annot_obj(self.active_record).symbol)
 
             pos_ind = sci.where(label != 'N')[0]
             neg_ind = sci.where(label == 'N')[0]
@@ -92,6 +94,6 @@ class ParseArrhythmiaSignals(object):
 
             return data
         
-    def to_json(self, record_num, n=100, channel=0):
+    def to_json(self, n=100, channel=0):
         """Return a sample of windows in JSON"""
-        return json.dumps(self.generate_data_batch(record_num, n, channel))
+        return json.dumps(self.generate_data_batch(self.active_record, n, channel))
